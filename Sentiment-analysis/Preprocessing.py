0df891bade674227cd94567792b62c1a640cafb6
Aleksandra Rozmus <aleksandra_rozmus00@outlook.com>
2023-11-12 10:30:45 +0100

    Add Preprocessing data

---
+++ Sentiment-analysis/Preprocessing.py
@@ -0,0 +1,53 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize.casual import casual_tokenize
+
+
+def lemmatisation(text):
+    """
+    This function is responsible for lemmatisation. Find the root form of words or lemmas in NLP and remove stop words.
+    :param text:
+    :return: String after lemmatisation
+    """
+
+    stop_words = set(stopwords.words('english'))
+    lemmatizer = nltk.stem.WordNetLemmatizer()
+    lemmatize_data = ""
+
+    tokens = casual_tokenize(str(text), reduce_len=True, strip_handles=True)
+    for word in tokens:
+        if not word in stop_words and len(word) > 0:
+            lemmatize_data = lemmatize_data + lemmatizer.lemmatize(word) + " "
+    return lemmatize_data
+
+
+def preprocessing_data(text):
+    """
+    This function preprocess the text:
+    1. everything apart from letters is excluded
+    2. multiple spaces are replaced by single space
+    3. data is converted to lower case
+    4. find the root form of words or lemmas in NLP and remove stop words
+
+    :param text:  text that should be preprocessed
+    :return: Preprocessed string
+    """
+    preprocess_data = re.sub(r'http\S+', ' ', str(text)) #delete http/https
+    preprocess_data = re.sub(r'(^| )\S+\.com( |$)', ' ', preprocess_data) #delete link.com
+    preprocess_data = re.sub(r'<\S+\W?/?>', ' ', preprocess_data) #delete html marks e.g <br><br/>
+    preprocess_data = re.sub(r'(@\w+|\d+)', ' ', preprocess_data, flags=re.IGNORECASE) #delete user names
+    preprocess_data = re.sub(r'[^a-z\s]+', ' ', preprocess_data, flags=re.IGNORECASE) #leave only letters and spaces
+    preprocess_data = re.sub(r'(\s+)', ' ', preprocess_data) #delete unnecesary spaces
+    preprocess_data = preprocess_data.lower()
+    preprocess_data = lemmatisation(preprocess_data)
+    preprocess_data = re.sub(r'[^a-z\s]+', ' ', preprocess_data, flags=re.IGNORECASE) #leave only letters and spaces
+    preprocess_data = re.sub(r'(^| ).( |$)', '', preprocess_data) #delete individual letters
+    preprocess_data = re.sub(r'(\s+)', ' ', preprocess_data) #delete unnecesary spaces
+
+    return preprocess_data
+
+
+def data_cleaning(data):
+    data = [preprocessing_data(data_str) for data_str in data]
+    return data
---
+++ Sentiment-analysis/main.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import warnings
+import matplotlib.pyplot as plt
+import Preprocessing
+from sklearn import model_selection
+import numpy as np
+
+if __name__ == '__main__':
+    warnings.filterwarnings("ignore")
+    # ------------------------- Upload data -----------------------------#
+
+    print("------------------------- Upload data -------------------------")
+    training_set = pd.read_csv('./data/IMDBDataset.csv', header=None, sep=',')
+    x_train = training_set[0].values[1:]  # content, delete headers
+    y_train = training_set[1].values[1:]  # sentiment, delete headers
+
+    # # -------------------------------------------------------------------#
+    #
+    # # ------------Plot The total number of examples in dataset----------#
+
+    plt.figure(1)
+    fig1, ax1 = plt.subplots()
+    N, bins, patches = ax1.hist(y_train, bins=3)
+    patches[0].set_facecolor('green')
+    patches[2].set_facecolor('red')
+    plt.grid(color='#CCCCCC', linestyle=':', linewidth=1)
+    plt.title('Liczba przykładów w zbiorze danych dla każdej kategorii sentymentu')
+    plt.xlabel("Kategoria sentymentu", fontsize=10)
+    plt.ylabel("Liczba przykładów", fontsize=10)
+    plt.savefig("images/1.png")
+    plt.show()
+
+    # # -------------------------------------------------------------------#
+    #
+    # # ------------------------Preprocessing data-------------------------#
+
+    # print("----------------------- Preprocessing data -----------------------")
+    x_train_after_preprocessing = []
+    y_train_after_preprocessing = []
+    for index in range(len(x_train)):
+        text = Preprocessing.preprocessing_data(x_train[index])
+        if text != "":
+            x_train_after_preprocessing.append(text)
+            y_train_after_preprocessing.append(y_train[index])
+
+    pd.DataFrame(x_train_after_preprocessing).to_csv("x_train.csv", header=None, sep=";")
+    pd.DataFrame(y_train_after_preprocessing).to_csv("y_train.csv", header=None, sep=";")
+
+    # # -------------------------------------------------------------------#
+    #
+    # # -------------Upload data after preprocessing data------------------#
+    #
+    print("------------------- Upload data after preprocessing data --------------------")
+    x_train_after_preprocessing = pd.read_csv('x_train.csv', header=None, sep=";")[1].values
+    y_train_after_preprocessing = pd.read_csv('y_train.csv', header=None, sep=";")[1].values
+
+    # # -------------------------------------------------------------------#
+    #
+    # # ------Plot The total number of examples after preprocessing--------#
+
+    plt.figure(2)
+    fig3, ax3 = plt.subplots()
+    N3, bins3, patches3 = ax3.hist([y_train, y_train_after_preprocessing],
+                                   label=['Przed przetworzeniem', 'Po przetworzeniu'], bins=3)
+    plt.legend(loc='upper right')
+    plt.grid(color='#CCCCCC', linestyle=':', linewidth=1)
+    plt.title('Liczba przykładów w zbiorze danych dla każdej kategorii sentymentu \n przed i po przetworzeniu danych')
+    plt.xlabel("Kategoria sentymentu", fontsize=10)
+    plt.ylabel("Liczba przykładów", fontsize=10)
+    plt.savefig("images/2.png")
+    plt.show()
+
+    # # -------------------------------------------------------------------#
+    #
+    # # ---------------------------Split data------------------------------#
+    train_data, test_data, train_labels, test_labels = model_selection.train_test_split(x_train_after_preprocessing,
+                                                                                        y_train_after_preprocessing,
+                                                                                        shuffle=True,
+                                                                                        test_size=0.25,
+                                                                                        random_state=42,
+                                                                                        stratify=y_train_after_preprocessing)
+    classes = np.unique(train_labels)
+
+    # # -------------------------------------------------------------------#
+    #
+    # # ------Plot The total number of training and test examples----------#
+
+    plt.figure(3)
+    fig24, ax4 = plt.subplots()
+    plt.grid(color='#CCCCCC', linestyle=':', linewidth=1)
+    height = [len(train_data), len(test_data)]
+    y_pos = np.arange(len(height))
+    bars = ["Dane treningowe", "Dane testowe"]
+    plt.bar(y_pos, height, color=['dodgerblue', 'darkorange'])
+    plt.xticks(y_pos, bars)
+    plt.title('Liczba przykładów treningowych i testowych')
+    plt.xlabel("Rodzaj danych", fontsize=10)
+    plt.ylabel("Liczba przykładów", fontsize=10)
+    rects = ax4.patches
+    des = ["75%", "25%"]
+    i = 0
+    for rect, height in zip(rects, height):
+        height_ann = rect.get_height()
+        ax4.text(
+            rect.get_x() + rect.get_width() / 2, height_ann, des[i], ha="center", va="bottom"
+        )
+        i = i + 1
+    plt.savefig("images/3.png")
+    plt.show()
